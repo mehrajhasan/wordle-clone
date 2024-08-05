8b8b72cd74d55874ea445e9789192c327e40ff08
Mehraj Hasan <mehraj@Mehrajs-MacBook-Air.local>
2024-08-05 19:46:47 -0400

    implemented

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,7 +1,24 @@
-export const Key = ({keyVal}) => {
-    <div className="key">
-        {keyVal}
-    </div>
+import { useContext } from 'react';
+import { AppContext } from '../App';
+
+export const Key = ({keyVal, bigKey}) => {
+    const { onEnter, onDelete, onSelectLetter } = useContext(AppContext);
+
+    const selectLetter = () => {
+        if(keyVal === "ENTER"){
+            onEnter();
+        } else if(keyVal === "DEL"){
+            onDelete();
+        }   else{
+            onSelectLetter(keyVal);
+        }   
+    };
+
+    return (
+        <div className="key" id={bigKey && "big"} onClick={selectLetter}>
+            {keyVal}
+        </div>
+    );
 }
 
 export default Key;
