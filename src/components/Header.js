b40dfbff566d4fd36b5d8390934c65c02be56c90
Mehraj Hasan <mehraj@Mehrajs-MacBook-Air.local>
2024-08-05 19:47:03 -0400

    slight changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import { Navbar } from 'react-bootstrap';
 export const Header = () => {
     return (
         <Navbar>
-            <h1>Wordle</h1>
+            <h1>Mehraj's Wordle</h1>
         </Navbar>
     );
 }
