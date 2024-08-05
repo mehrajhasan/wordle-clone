4a8e10bc9df40cf572d34ac2d6c6ec673f8663ad
Mehraj Hasan <mehraj@Mehrajs-MacBook-Air.local>
2024-08-05 19:47:45 -0400

    proper functionality added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-
 import Header from './components/Header';
 import Board from './components/Board';
 import Keyboard from './components/Keyboard';
@@ -10,13 +9,41 @@ export const AppContext = createContext();
 
 function App() {
   const [board,setBoard] = useState(boardDefault);
+  const [currAttempt, setCurrAttempt] = useState({attempt:0, letterPos:0});
+
+  const onSelectLetter = (keyVal) => {
+    if(currAttempt.letterPos > 4)
+      return;
+
+    const currBoard = [...board];
+    currBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
+    setBoard(currBoard);
+    setCurrAttempt({...currAttempt, letterPos: currAttempt.letterPos+1});
+  };
+
+  const onDelete = (keyVal) => {
+    if(currAttempt.letterPos === 0)
+      return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos-1]="";
+    setBoard(newBoard)
+    setCurrAttempt({...currAttempt,letterPos:currAttempt.letterPos-1})
+  };
+
+  const onEnter = () => {
+    if(currAttempt.letterPos!==5)
+      return;
+    setCurrAttempt({ attempt: currAttempt.attempt+1,letterPos:0 })
+  };
 
   return (
     <div className="App">
       <Header/>
-      <AppContext.Provider value={{board,setBoard}}>
-        <Board/>
-        <Keyboard/>
+      <AppContext.Provider value={{board,setBoard,currAttempt,setCurrAttempt,onEnter,onDelete,onSelectLetter}}>
+        <div className="box">
+          <Board/>
+          <Keyboard/>
+        </div>
       </AppContext.Provider>
     </div>
   );
