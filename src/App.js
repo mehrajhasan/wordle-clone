0c7fd589a215ec63de769f0d30556ac74b447c84
Mehraj Hasan <mehraj@Mehrajs-MacBook-Air.local>
2024-08-05 16:55:41 -0400

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Header from './components/Header';
+import Board from './components/Board';
+import Keyboard from './components/Keyboard';
+import { boardDefault } from './Words';
+import { createContext, useState } from 'react';
+
+export const AppContext = createContext();
+
 function App() {
+  const [board,setBoard] = useState(boardDefault);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <AppContext.Provider value={{board,setBoard}}>
+        <Board/>
+        <Keyboard/>
+      </AppContext.Provider>
     </div>
   );
 }
