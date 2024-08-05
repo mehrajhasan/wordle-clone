105aac71e3cbbdcd487d806d3e4b2509b5d7bdd8
Mehraj Hasan <mehraj@Mehrajs-MacBook-Air.local>
2024-08-05 16:56:59 -0400

    header done, may add

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import { Navbar } from 'react-bootstrap';
+export const Header = () => {
+    return (
+        <Navbar>
+            <h1>Wordle</h1>
+        </Navbar>
+    );
+}
+
+export default Header;
