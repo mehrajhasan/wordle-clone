3ea8d0699b975e0c22049210604dacd1cbaece38
Mehraj Hasan <mehraj@Mehrajs-MacBook-Air.local>
2024-08-07 22:36:02 -0400

    handled new sets for new games and new word etc

--- src/Words.js
+++ src/Words.js
@@ -1,3 +1,5 @@
+import wordBank from './wordle-bank.txt';
+
 export const boardDefault = () => [
     ['','','','',''],
     ['','','','',''],
@@ -6,5 +8,17 @@ export const boardDefault = () => [
     ['','','','',''],
     ['','','','',''],
 ];
+
+export const generateWordSet = async () => {
+    let wordSet;
+    let todaysWord;
+    await fetch(wordBank).then((response) => response.text()).then((result) => {
+        const wordArr = result.split("\n");
+        todaysWord = wordArr[Math.floor(Math.random()*wordArr.length)]
+        wordSet = new Set(wordArr);
+    });
+
+    return { wordSet, todaysWord};
+};
 
 export default boardDefault;
