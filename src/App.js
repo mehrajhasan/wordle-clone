8a44aa2d1980d9b15f2a911894999224ec9083c3
Mehraj Hasan <mehraj@Mehrajs-MacBook-Air.local>
2024-08-07 22:35:00 -0400

    completed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,36 @@ import './App.css';
 import Header from './components/Header';
 import Board from './components/Board';
 import Keyboard from './components/Keyboard';
-import { boardDefault } from './Words';
-import { createContext, useState } from 'react';
+import End from './components/End';
+import { boardDefault, generateWordSet } from './Words';
+import { createContext, useState, useEffect } from 'react';
 
 export const AppContext = createContext();
 
 function App() {
   const [board,setBoard] = useState(boardDefault);
   const [currAttempt, setCurrAttempt] = useState({attempt:0, letterPos:0});
+  const [wordSet, setWordSet] = useState(new Set());
+  const [disabledLetters, setDisabledLetters] = useState([]);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [almostLetters, setAlmostLetters] = useState([]);
+  const [correctWord, setCorrectWord] = useState("");
+  const [end, setEnd] = useState({end: false, guessedWord: false});
+  const [modal, setModal] = useState(false);
+
+  useEffect(() => {
+    generateWordSet().then((words) => {
+      setWordSet(words.wordSet);
+      setCorrectWord(words.todaysWord);
+    });
+
+  }, []);
+
+  useEffect(() => {
+    if(end.end){
+      setTimeout(() => setModal(true), 2000);
+    }
+  }, [end])
 
   const onSelectLetter = (keyVal) => {
     if(currAttempt.letterPos > 4)
@@ -21,7 +43,7 @@ function App() {
     setCurrAttempt({...currAttempt, letterPos: currAttempt.letterPos+1});
   };
 
-  const onDelete = (keyVal) => {
+  const onDelete = () => {
     if(currAttempt.letterPos === 0)
       return;
     const newBoard = [...board];
@@ -33,16 +55,41 @@ function App() {
   const onEnter = () => {
     if(currAttempt.letterPos!==5)
       return;
-    setCurrAttempt({ attempt: currAttempt.attempt+1,letterPos:0 })
+
+    let currWord = "";
+    for(let x=0;x<5;x++){
+      currWord+=board[currAttempt.attempt][x];
+    }
+
+    if(wordSet.has(currWord.toLowerCase())){
+      setCurrAttempt({ attempt: currAttempt.attempt+1,letterPos:0 })
+    } else{
+      alert("Not in word list!")
+    }
+
+    if(currWord.toUpperCase() === correctWord.toUpperCase()){
+      console.log("correct word")
+      setEnd({end: true, guessedWord: true});
+      return;
+    }
+
+    if(currAttempt.attempt===5){
+      console.log("u lost")
+      setEnd({end: true, guessedWord: false});
+      return;
+    }
+
   };
+
+  console.log(correctWord)
 
   return (
     <div className="App">
       <Header/>
-      <AppContext.Provider value={{board,setBoard,currAttempt,setCurrAttempt,onEnter,onDelete,onSelectLetter}}>
+      <AppContext.Provider value={{board,setBoard,currAttempt,setCurrAttempt,onEnter,onDelete,onSelectLetter, correctWord, disabledLetters, setDisabledLetters, correctLetters, setCorrectLetters, almostLetters, setAlmostLetters, end}}>
         <div className="box">
           <Board/>
-          <Keyboard/>
+          {end.end ? <End/> : <Keyboard/>}
         </div>
       </AppContext.Provider>
     </div>
